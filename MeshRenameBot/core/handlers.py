b845b9037dd888603d3ed41a529431bafa26fc20
Aaman <heyaaman2004@gmail.com>
2022-03-25 21:03:56 +0530

    Update handlers.py

--- MeshRenameBot/core/handlers.py
+++ MeshRenameBot/core/handlers.py
@@ -48,7 +48,7 @@ def add_handlers(client: Client) -> None:
 
 
 async def start_handler(client: Client, msg: Message) -> None:
-    await msg.reply(Trans.START_MSG, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Updates Channel", url=f"https://t.me/ReGALTOs_BotZ"),InlineKeyboardButton("Support", url=f"https://t.me/ReGALTOs_Botz_SupporT")]]),)
+    await msg.reply(Trans.START_MSG, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Updates Channel", url=f"https://t.me/ReGALTOs_BotZ"),InlineKeyboardButton("Help", callback_data="help_str")]]),)
 
 
 async def rename_handler(client: Client, msg: Message) -> None:
